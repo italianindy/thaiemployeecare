69ef291499a03bfff79041bf36d945023595dcc1
italianindy <chayutwork@gmail.com>
2015-09-28 14:35:30 +0700

    Update 1.1.1
    Update 1.1.1

--- detailemployee.php
+++ detailemployee.php
@@ -2,7 +2,7 @@
 session_start();
 if($_SESSION["strUserID"] == "")
 {
-header("location:mainmenu.php");
+header("location:http://112.121.150.67/thaiemployeecare/mainmenu.php");
 exit();
 }
 ?>
@@ -16,7 +16,7 @@ exit();
 <script src="jquery-mobile/jquery-1.6.4.min.js" type="text/javascript"></script>
 <script src="jquery-mobile/jquery.mobile-1.0.min.js" type="text/javascript"></script>
 </head> 
-<body> 
+<body>
  
 <?
 $objConnect = mysql_connect("112.121.150.67","hdc","hdc") or die(mysql_error());
@@ -24,11 +24,7 @@ $objConnect = mysql_connect("112.121.150.67","hdc","hdc") or die(mysql_error());
 $objDB = mysql_select_db("db_employee");
 mysql_query("SET NAMES utf8", $objConnect);
 
-$strSQL = " SELECT
-a.*, b.*, c.*, d.*
-FROM employee a INNER JOIN (branch b,department c, employeecome d)
-ON (b.branch_id=a.branch_id AND a.dep_id=c.dep_id AND a.emp_id = d.emp_id)
-WHERE b.branch_id = 'CCH4444444'";
+$strSQL = " SELECT * FROM branch  WHERE branch_id = '".$_GET["bid"]."'";
 
 
 $objQuery = mysql_query($strSQL) or die (mysql_error());
@@ -36,44 +32,120 @@ $objQuery = mysql_query($strSQL) or die (mysql_error());
 $objResult = mysql_fetch_array($objQuery);
 ?>
 
-<div data-role="page" id="page">
-	
-    <div data-role="header" data-theme="e">
-    <a href="http://112.121.150.67/thaistudentcare/mainparent.php" data-icon="back" data-iconpos="notext" data-direction="reverse" >Back</a>
-		<h1><font size="2" >ผู้ปกครอง : คุณ<? echo $objResult["parent_fname"];?> <? echo $objResult["parent_lname"];?></font></h1>
+<div data-role="page" data-theme="e" id="pageMparent">
+	<div data-role="header" data-theme="a">
+    <a href="http://112.121.150.67/thaiemployeecare/mainemployee.php" data-icon="back" data-iconpos="notext" data-direction="reverse" >Back</a>
+		<h1><font size="3" >สาขา : <? echo $objResult["branch_name"];?> </font></h1>
 	</div>
+<?
+$strSQL2 = "SELECT
+employeecome.emp_id,
+employeecome.emc_in,
+date_format(employeecome.emc_in, '%H:%i:%s'),
+employee.chap_id,
+chapter.chap_in,
+chapter.chap_late,
+if (date_format(employeecome.emc_in, '%H:%i:%s') > date_format(chap_late, '%H:%i:%s'),1,0) AS Late, branch.branch_id, branch.branch_name,employee.emp_prefix,employee.emp_fname, employee.emp_lname
+FROM
+employeecome ,
+employee
+LEFT JOIN (
+						select chapter.chap_id
+						 ,chapter.chap_in
+						 ,chapter.chap_late
+						 from chapter    
+					) AS chapter ON chapter.chap_id= employee.chap_id ,
+branch
+WHERE
+branch.branch_id = '".$_GET["bid"]."' AND
+employee.emp_id = employeecome.emp_id AND
+employee.emp_status = 'ปกติ' AND
+branch.branch_id = employee.branch_id
+AND
+date_format(employeecome.emc_in,'%Y-%m-%d') = date_format(now(),'%Y-%m-%d') 
+
+ ";
+$objQuery2 = mysql_query($strSQL2) or die (mysql_error());
+?>   
+
 	<div data-role="content">	
-		<div style="padding-left:10px;padding-right:10px">
- 
-<div align="right">
-      <img src="pic/datastud.png" width="250" height="80"> 			</div>
- 
- 
-	<div data-role="fieldcontain">
-		<label for="name">เลขบัตรประชาชนนักเรียน :</label>
-			<? echo $objResult["stud_id"];?>
-	</div>
-	<div data-role="fieldcontain">
-		<label for="name">ชื่อ-สกุล :</label>
-			<? echo $objResult["stud_prefix"];?><? echo $objResult["stud_fname"];?> <? echo $objResult["stud_lname"];?>
-	</div>
-    <div data-role="fieldcontain">
-		<label for="name">ระดับชั้น : มัธยมศึกษาปีที่ </label>
-			<? echo $objResult["level_name"];?>
-	</div>
+    <div align="center">
+      <img src="pic/choosebrancht1.png" width="250" height="80"> 			</div>
+		<ul data-inset="true"  data-role="listview" data-theme="a">
+        <li data-role="list-divider">รายชื่อพนักงานเข้าสาย</li>
+<?
+while($objResult2 = mysql_fetch_array($objQuery2)) {
+	if($objResult2["Late"] == "1"){
+?>
+  <li><a href="#">
+    <h3><font size="3" ><? echo $objResult2["emp_prefix"]?></font><font size="3" ><? echo $objResult2["emp_fname"]?> </font> <font size="3" ><? echo $objResult2["emp_lname"]?></font></h3>
     
-	<div data-role="fieldcontain">
-		<label for="name">วันที่และเวลาเข้าเรียนล่าสุด :</label>
-			<? echo $objResult["stc_in"];?>
-	</div>
-	<div data-role="fieldcontain">
-		<label for="name">วันที่และเวลากลับล่าสุด :</label>
-			<? echo $objResult["stc_out"];?>
-	</div>
-        <font size="4" color="#990000"><a href="http://112.121.150.67/thaistudentcare/mainparent.php" data-icon="star" data-role="button" data-theme="e">ดูอย่างละเอียด</a></font>
-				
+  </a><a href="#">รายละเอียด</a></li>
+<?
+	}
+}
+?> 
+</ul>
+<?
+/*
+$strSQL3 = "SELECT
+employeecome.emp_id,
+employeecome.emc_in,
+date_format(employeecome.emc_in, '%H:%i:%s'),
+employee.chap_id,
+chapter.chap_in,
+chapter.chap_late,
+if (date_format(employeecome.emc_in, '%H:%i:%s') > date_format(chap_late, '%H:%i:%s'),1,0) AS Late, branch.branch_id, branch.branch_name,employee.emp_prefix,employee.emp_fname, employee.emp_lname
+FROM
+employeecome ,
+employee
+LEFT JOIN (
+						select chapter.chap_id
+						 ,chapter.chap_in
+						 ,chapter.chap_late
+						 from chapter    
+					) AS chapter ON chapter.chap_id= employee.chap_id ,
+branch
+WHERE
+branch.branch_id = '".$_GET["bid"]."' AND
+employee.emp_id = employeecome.emp_id AND
+employee.emp_status = 'ปกติ' AND
+branch.branch_id = employee.branch_id
+AND
+date_format(employeecome.emc_in,'%Y-%m-%d') = date_format(now(),'%Y-%m-%d') 
 
-
+ ";
+ */
+$strSQL2 =  "select employee.*
+from employee
+where employee.emp_id not in (select emp_id from employeecome where date_format(employeecome.emc_in,'%Y-%m-%d') = date_format(now(),'%Y-%m-%d'))
+and employee.emp_status = 'ปกติ'
+and employee.branch_id = '".$_GET["bid"]."'";
+$objQuery3 = mysql_query($strSQL2) or die (mysql_error());
+?>   
+<ul data-inset="true"  data-role="listview" data-theme="a">
+        <li data-role="list-divider">รายชื่อพนักงานที่ยังไม่เข้างาน</li>
+<?
+while($objResult3 = mysql_fetch_array($objQuery3)) {
+?>
+  <li><a href="#">
+    <h3><font size="3" ><? echo $objResult3["emp_prefix"]?></font><font size="3" ><? echo $objResult3["emp_fname"]?> </font> <font size="3" ><? echo $objResult3["emp_lname"]?></font></h3>
+    
+  </a><a href="#">รายละเอียด</a></li>
+<?
+}
+?> 
+</ul>
+	</div>
+    <div data-role="footer" data-theme="a" data-position="fixed">
+		<div data-role="navbar">
+          <ul>
+            <li><a href="http://112.121.150.67/thaiemployeecare/logout.php"><font size="4">ออกจากระบบ</font></a></li>
+           
+          </ul>
+        </div>
+  </div>
+</div>
 
 </body>
 </html>
--- mainemployee.php
+++ mainemployee.php
@@ -28,6 +28,16 @@ $strSQL = " SELECT * FROM employee  WHERE emp_id = '".$_SESSION["strUserID"]."'"
 $objQuery = mysql_query($strSQL) or die (mysql_error());
 
 $objResult = mysql_fetch_array($objQuery);
+
+$sql = "select branch_id,count(*)
+from employee
+group by branch_id";
+$objQuery3 = mysql_query($sql) or die (mysql_error());
+while($objResult3 = mysql_fetch_array($objQuery3)) {
+	$TotalEmp[$objResult3[0]] = $objResult3[1];
+}
+
+
 ?>
 
 <div data-role="page" data-theme="e" id="pageMparent">
@@ -36,21 +46,53 @@ $objResult = mysql_fetch_array($objQuery);
 		<h1><font size="2" >ชื่อผู้ใช้ : คุณ<? echo $objResult["emp_fname"];?> <? echo $objResult["emp_lname"];?></font></h1>
 	</div>
 <?
-$strSQL2 = "SELECT * 
-FROM branch ";
+$strSQL2 = "SELECT
+Count(*) AS ComeCount,
+Sum(if(date_format(employeecome.emc_in,'%H:%i:%s') > date_format(chap_late,'%H:%i:%s'),1,0)) AS LateCount, 
+branch.branch_name, branch.branch_id, COUNT(employee.emp_id) AS NumEmp
+FROM
+employeecome ,
+employee
+LEFT JOIN (
+						select chapter.chap_id
+						 ,chapter.chap_in
+						 ,chapter.chap_late
+						 from chapter    
+					) AS chapter ON chapter.chap_id= employee.chap_id ,
+branch
+WHERE
+employee.emp_id = employeecome.emp_id AND
+employee.emp_status = 'ปกติ' AND
+date_format(employeecome.emc_in,'%Y-%m-%d') = date_format(now(),'%Y-%m-%d') AND
+branch.branch_id = employee.branch_id
+GROUP BY
+employee.branch_id
+
+ ";
 $objQuery2 = mysql_query($strSQL2) or die (mysql_error());
 ?>   
 
 	<div data-role="content">	
     <div align="center">
-      <img src="pic/choosebrancht.png" width="250" height="80"> 			</div>
-		<ul data-inset="true" data-role="listview" data-theme="a">
+      <img src="pic/mainemployee.png" width="270" height="80"> 			</div>
+		<ul data-inset="true"  data-role="listview" data-theme="a">
 <?
 while($objResult2 = mysql_fetch_array($objQuery2)) {
+	if(isset($TotalEmp[$objResult2["branch_id"]])){		
+		$leave = $TotalEmp[$objResult2["branch_id"]] - $objResult2["ComeCount"];
+		$total = $TotalEmp[$objResult2["branch_id"]];
+	}else{
+		$leave = 0 - $objResult2["ComeCount"];
+		$total = 0;
+	}
+	
+		
+	
+	 
 ?>
   <li><a href="#">
-    <h3><font size="4" >สาขา<? echo $objResult2["branch_name"]?></font></h3>
-    <p><font size="3" >เลขที่สาขา </font><font size="2" ><? echo $objResult2["branch_id"]?></font></p>
+    <h3><font size="4" >สาขา<? echo $objResult2["branch_name"]?> </font><font size="3" >พนักงานทั้งหมด </font><font size="2" ><? echo $total;?></font> <font size="3" >คน</font></h3>
+    <p> <font size="3" >เข้างานแล้ว </font><font size="2" ><? echo $objResult2["ComeCount"]?> <font size="3" >คน</font>  </font> <font size="3" >เข้าสาย </font><font size="2" ><? echo $objResult2["LateCount"]?> <font size="3" >คน</font>  </font> <font size="3" >ยังไม่มา </font><font size="2" ><? echo $leave;?> <font size="3" >คน</font></font></p>
   </a><a href="http://112.121.150.67/thaiemployeecare/detailemployee.php?bid=<?=$objResult2["branch_id"];?>">รายละเอียด</a></li>
 <?
 }
